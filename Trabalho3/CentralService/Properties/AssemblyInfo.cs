de54a57a1b22278079421b9847253ab513bc2d8b
Luis Duarte <lduarte@luisduarte.net>
2013-01-17 16:12:32 +0000

    Added basic non-working version of the WCF Services. TODO: Put Ninject working properly.

--- Trabalho3/CentralService/Properties/AssemblyInfo.cs
+++ Trabalho3/CentralService/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CentralService")]
+[assembly: AssemblyTitle("CentralSupplierService")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CentralService")]
+[assembly: AssemblyProduct("CentralSupplierService")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
